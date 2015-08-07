d69e3e815a96a7c82f6e34543278f09fc516b94b
zacay1 <josehidalgo129@hotmail.com>
2015-08-06 22:51:12 -0430

    métodos para escapar cadenas

--- sc-caeb2/src/com/caeb2/util/TextUtils.java
+++ sc-caeb2/src/com/caeb2/util/TextUtils.java
@@ -1,5 +1,6 @@
 package com.caeb2.util;
 
+import java.io.UnsupportedEncodingException;
 import java.text.MessageFormat;
 
 public class TextUtils {
@@ -11,5 +12,33 @@ public class TextUtils {
 	public static boolean isEmptyOrNull(String text) {
 		return ((text == null) || (text.isEmpty()) || (text.trim().isEmpty()));
 	}
+	
+	public static String escaparString(String data) throws UnsupportedEncodingException{
+		
+		if(data==null){
+			return null;
+		}
+		
+		String newData=new String(data.getBytes("ISO-8859-1"),"UTF-8");
+		newData=newData.replace(",","\\,");
+		return newData;
+	}
+	
+	public static String[] escaparArray(String[] data) throws UnsupportedEncodingException{
+		
+		if(data==null){
+			return null;
+		}
+		
+		String newData[]= new String[data.length];
+		
+		for (int i = 0; i < data.length; i++) {
+			String string=new String(data[i].getBytes("ISO-8859-1"),"UTF-8").replace(",","\\,");
+			newData[i]=string;
+		}
+
+		return newData;
+	}
+	
 
 }
